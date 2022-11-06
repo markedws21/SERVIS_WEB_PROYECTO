e3f0e203cab8970e28fa7d6cb83e3abc2d5079eb
markedws21 <espinozarojasmark@gmail.com>
2022-11-06 12:21:45 -0500

    CAMBIOS ALUMNO

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -16,11 +16,12 @@ namespace SERVIS_WEB_PROYECTO.Controllers
     {
         // GET: Alumno
         AlumnoDAO alum = new AlumnoDAO();
+
         public ActionResult Index()
         {
             return View(alum.AlumnoListar().ToList());
         }
-        
+
         public ActionResult Details(int id)
         {
             return View(alum.BuscarAlumno(id));
